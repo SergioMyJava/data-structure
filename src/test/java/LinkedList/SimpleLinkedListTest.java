02a6a3f28bb5922cb8b8fbdf1db1b40db92c2844
Sergio1234567z <raboch2014@mail.ru>
2019-05-04 19:32:39 +0300

    Change JUnit4 to JUnit5

--- src/test/java/LinkedList/SimpleLinkedListTest.java
+++ src/test/java/LinkedList/SimpleLinkedListTest.java
@@ -1,9 +1,11 @@
 package LinkedList;
 
 import org.junit.Before;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
@@ -12,10 +14,11 @@ public class SimpleLinkedListTest<V> {
 
     private SimpleList exp = new SimpleLinkedList();
 
-    @Test
-    @Before
+    @org.junit.jupiter.api.Test
+    @BeforeEach
     @DisplayName("Must return true")
-    public void add() {
+
+    void add() {
         assertTrue(exp.add(12));
         assertTrue(exp.add(1));
         assertTrue(exp.add(2));
@@ -25,18 +28,18 @@ public class SimpleLinkedListTest<V> {
         assertTrue(exp.add(0));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     @DisplayName("Must return value by index")
-    public void get() {
+    void get() {
         assertEquals(12, exp.get(0));
         assertEquals(-5, exp.get(5));
         assertEquals(0, exp.get(6));
         assertEquals(1, exp.get(1));
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     @DisplayName("Must return true")
-    public void delete() {
+    void delete() {
         assertTrue(exp.delete(0));
         assertTrue(exp.delete(2));
         assertTrue(exp.delete(3));
@@ -44,7 +47,7 @@ public class SimpleLinkedListTest<V> {
         assertEquals(3,exp.getSize());
     }
 
-    @Test
+    @org.junit.jupiter.api.Test
     @DisplayName("Must Exaption")
     public void shouldThrowException() {
         Assertions.assertThrows(IndexOutOfBoundsException.class,() ->  exp.get(8));
