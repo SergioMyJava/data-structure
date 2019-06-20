41bc27f114bd08cd62d23102fb2af81ffe0e1e17
Sergio1234567z <raboch2014@mail.ru>
2019-06-20 16:53:48 +0300

    Add next array with duplicate numbers

--- src/main/java/mergeSort/App.java
+++ src/main/java/mergeSort/App.java
@@ -9,13 +9,11 @@ public class App {
 //     for(int v : b){
 //         System.out.println(v);
 //     }
-        int[] a = {3,6,1,5,7,22};
-        int[] b = {1,3,5,6,7,22};
+        int[] a = {3,6,1,5,90,90,7,22};
+        int[] b = {1,3,5,6,7,22,90,90};
         int[] c = Sort.sort(a);
         for(int k =0;k<b.length;k++){
-            System.out.println(a[k] == b[k]);
-            System.out.println(" ");
-            System.out.println(c[k] == b[k]);
+            System.out.println(b[k] == c[k]);
         }
     }
 }
